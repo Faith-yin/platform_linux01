71e9a1e80807a4573ee18c5d638d59819a1674bc
yinpengfei <1207828715@qq.com>
2020-03-07 16:30:20 +0800

    video样式书写

--- src/components/business/ListVideo.vue
+++ src/components/business/ListVideo.vue
@@ -5,7 +5,19 @@
 -->
 <template>
   <div id="video-list">
-    333
+    <div v-for="(item,index) in arr" :key="index" class="video-item">
+      <div class="title-wrapper">
+        <div class="title" @click="$emit('listClick',index)">{{item.title}}</div>
+      </div>
+      <div class="content-wrapper">
+        <div class="content">{{item.content}}</div>
+      </div>
+      <div class="bottom-wrapper">
+        <div class="mr--30">添加者：{{item.author}}</div>
+        <div class="mr--30">上传日期：{{item.date}}</div>
+        <div>浏览次数：{{item.viewCount}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,7 +25,17 @@
 export default {
   data() {
     return {
-      
+      arr: [
+        {title: '这是视频标题1', content: '这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，', link: 'https://www.mi.com', author: '殷鹏飞',viewCount: 29, date: '2020-03-07 15:15:00'},
+        {title: '这是视频标题2', content: '这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，', link: 'https://www.mi.com', author: '殷鹏飞',viewCount: 29, date: '2020-03-07 15:15:00'},
+        {title: '这是视频标题3', content: '这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，', link: 'https://www.mi.com', author: '殷鹏飞',viewCount: 29, date: '2020-03-07 15:15:00'},
+        {title: '这是视频标题4', content: '这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，', link: 'https://www.mi.com', author: '殷鹏飞',viewCount: 29, date: '2020-03-07 15:15:00'},
+        {title: '这是视频标题5', content: '这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，', link: 'https://www.mi.com', author: '殷鹏飞',viewCount: 29, date: '2020-03-07 15:15:00'},
+        {title: '这是视频标题6', content: '这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，', link: 'https://www.mi.com', author: '殷鹏飞',viewCount: 29, date: '2020-03-07 15:15:00'},
+        {title: '这是视频标题7', content: '这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，', link: 'https://www.mi.com', author: '殷鹏飞',viewCount: 29, date: '2020-03-07 15:15:00'},
+        {title: '这是视频标题8', content: '这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，', link: 'https://www.mi.com', author: '殷鹏飞',viewCount: 29, date: '2020-03-07 15:15:00'},
+        {title: '这是视频标题9', content: '这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，这是视屏的内容，想学习的小伙伴可以点进去哦，', link: 'https://www.mi.com', author: '殷鹏飞',viewCount: 29, date: '2020-03-07 15:15:00'},
+      ]
     }
   },
   methods: {
@@ -23,5 +45,32 @@ export default {
 </script>
 
 <style lang="scss">
-
+#video-list {
+  .video-item {
+    margin: 10px 0;
+    padding: 18px;
+    box-sizing: border-box;
+    border-radius: 5px;
+    background-color: #fff;
+    .title-wrapper {
+      .title {
+        font-size: 20px;
+        font-weight: 500;
+        &:hover {
+          cursor: pointer;
+          color: #d33a2a;
+        }
+      }
+    }
+    .content-wrapper {
+      margin: 15px 0;
+      font-size: 15px;
+    }
+    .bottom-wrapper {
+      display: flex;
+      font-size: 14px;
+      color: #999;
+    }
+  }
+}
 </style>
--- src/views/DetailVideo.vue
+++ src/views/DetailVideo.vue
@@ -4,7 +4,31 @@
  * @Description: 视频详情页面
 -->
 <template>
-  <div>
+  <div id="detail-video">
+    <!-- 顶部区域 -->
+    <div class="top-wrapper">
+      <div class="title-info">
+        <img src="@/assets/img/linux-logo.jpg" alt="linux_logo">
+        <div class="top-title">Linux 管理学习平台</div>
+      </div>
+    </div>
+    <!-- 页头 -->
+    <div class="header-wrapper">
+      <el-page-header @back="goBack" content="视频详情"></el-page-header>
+    </div>
+    <!-- 主体区域 -->
+    <div class="body-wrapper">
+      <div class="title-wrapper">{{obj.title}}</div>
+      <div class="info-wrapper mb--20">
+        <div class="one-info mr--30"><i class="el-icon-user"></i> {{obj.author}}</div>
+        <div class="two-info mr--30"><i class="el-icon-date"></i> {{obj.date}}</div>
+        <div><i class="el-icon-view"></i> {{obj.viewCount}}次浏览</div>
+      </div>
+      <div class="content-wrapper">{{obj.content}}</div>
+      <div class="link-wrapper">
+        <video :src="obj.link" controls="controls"></video>
+      </div>
+    </div>
 
   </div>
 </template>
@@ -17,11 +41,21 @@ export default {
   mixins: [publicInfo, publicClass],
   data() {
     return {
-
+      // 详情信息
+      obj: {title: 'linux深度学习链接',date: '2020-03-07 15:15:00', author: '殷鹏飞',link: 'http://centos/group1/M00/00/00/wKiAgl5jVYKAXL2CAA1KgDVRfmo797.mp4',content: '想学习的可以点进去学习哦',viewCount: 11}
     }
   },
   methods: {
-    
+    /**
+     * @Author: 殷鹏飞
+     * @Date: 2020-03-07 15:20:45
+     * @Description: 返回事件
+     */
+    goBack() {
+      this.routeGo({
+        name: 'HomePage'
+      })
+    },
   },
 }
 </script>
