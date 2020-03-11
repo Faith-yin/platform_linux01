5c9aff2937818a78a06bd8897874e3c76be1b2b8
yinpengfei <1207828715@qq.com>
2020-03-11 21:17:51 +0800

    文件视频上传

--- src/mixins/public_class.js
+++ src/mixins/public_class.js
@@ -74,28 +74,6 @@ export default {
             return mark
         },
         /**
-         * @author: 殷鹏飞
-         * @Date: 2019-12-27 08:47:26
-         * @information: 获取当前年月日
-         */
-        getCurrentDate() {
-            // 获取当前日期
-            let date = new Date()
-            // 获取当前月份
-            let nowMonth = date.getMonth() + 1
-            // 获取当前是几号
-            let strDate = date.getDate()
-            // 对月份进行处理，1-9月在前面添加一个“0”
-            if (nowMonth >= 1 && nowMonth <= 9) {
-                nowMonth = `0${nowMonth}`
-            }
-            // 对月份进行处理，1-9号在前面添加一个“0”
-            if (strDate >= 0 && strDate <= 9) {
-                strDate = `0${strDate}`
-            }
-            this.timeNow = `${date.getFullYear()}-${nowMonth}-${strDate}`
-        },
-        /**
          * @Author: 殷鹏飞
          * @Date: 2020-03-09 10:26:16
          * @Description: 格式化时间
